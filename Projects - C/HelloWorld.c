41563e69af7a2c8e1e6d4941d791d234b510f2a6
MichaelVerthin <michaelvn5202@gmail.com>
2023-10-30 22:58:38 +0200

    Study session 30/10

--- Projects - C/HelloWorld.c
+++ Projects - C/HelloWorld.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+// # define TAXES = 0.17
 void main()
 {
-    printf("Hello World");
+    printf("\aHello Werld!\n");
+    int x;
+    printf("Please Enter a number: ");
+    scanf("%d", &x);
+    printf("%d is a number\n", x);
+
+    float price, totalPrice;
+    const float TAXES = 0.17;
+    printf("Please enter the product price: ");
+    scanf("%f", &price);
+    totalPrice = price + (price * TAXES);
+    printf("Total price including %.2f%% taxes is %.2f\n", TAXES * 100, totalPrice);
 }
