b664418bd6094667760be327f88a8f602dd87ee1
MichaelVerthin <michaelvn5202@gmail.com>
2023-12-23 00:26:41 +0200

    Some changes to Doll

--- Projects - C/doll/doll.c
+++ Projects - C/doll/doll.c
@@ -2,22 +2,12 @@
 
 int main()
 {
-  /*
-  float floatArr[10];
-  int i = 0;
-  while(scanf("%f", &floatArr[i])==1)
-  {
-    printf("The first number is: %.2f", floatArr[0]);
-    i++;
-  }
-  */
-  
   float dollar_exchange_rate = 0, total_dollar = 0, total_shekel = 0, dollar_amount = 0;
   int i = 0;
-  
-  while(scanf("%f", &dollar_amount)==1)
+
+  while (scanf("%f", &dollar_amount) == 1)
   {
-    if(i==0)
+    if (i == 0)
     {
       printf("$\t\tIS\n");
       dollar_exchange_rate = dollar_amount;
@@ -30,7 +20,7 @@ int main()
     }
     i++;
   }
-  
+
   printf("%.2f\t\t%.2f\n", total_dollar, total_shekel);
   return 0;
 }
